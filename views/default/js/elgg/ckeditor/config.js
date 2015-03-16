def6467096108e25cb7d973341c66cc0a43edda9
Jeff Tilson <jrtilson@gmail.com>
2015-03-16 17:18:12 -0400

    Added fonts and justify plugins, added more items to the toolbar

--- views/default/js/elgg/ckeditor/config.js
+++ views/default/js/elgg/ckeditor/config.js
@@ -2,16 +2,18 @@ define(function(require) {
 	var elgg = require('elgg');
 	var $ = require('jquery');
 
-	// Add the onchange plugin
+	// Add extra plugins
 	CKEDITOR.plugins.addExternal('onchange', elgg.get_site_url() + 'mod/tgsutilities/views/default/js/elgg/ckeditor/onchange.js', '');
+	CKEDITOR.plugins.addExternal('justify', elgg.get_site_url() + 'mod/tgsutilities/views/default/js/elgg/ckeditor/justify/plugin.js', '');
+	CKEDITOR.plugins.addExternal('font', elgg.get_site_url() + 'mod/tgsutilities/views/default/js/elgg/ckeditor/font/plugin.js', '');
 
 	return {
-		toolbar: [['Bold', 'Italic', 'Underline', 'RemoveFormat'], ['Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
+		toolbar: [['Bold', 'Italic', 'Underline', 'Strike', 'RemoveFormat'], ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'Indent', 'Outdent'],[ 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize'], ['Format', 'FontSize']],
 		removeButtons: 'Subscript,Superscript', // To have Underline back
 		allowedContent: true,
 		baseHref: elgg.config.wwwroot,
 		removePlugins: 'contextmenu,tabletools,resize',
-		extraPlugins: 'blockimagepaste,onchange',
+		extraPlugins: 'blockimagepaste,onchange,justify,font',
 		defaultLanguage: 'en',
 		language: elgg.config.language,
 		skin: 'moono',
