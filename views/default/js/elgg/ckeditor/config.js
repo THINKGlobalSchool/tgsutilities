e1fefbf20ac061505d8cf19ee9deca98c8f67630
Jeff Tilson <jrtilson@gmail.com>
2015-09-11 10:00:35 -0400

    Added CMD+K shortcut for linking

--- views/default/js/elgg/ckeditor/config.js
+++ views/default/js/elgg/ckeditor/config.js
@@ -12,7 +12,7 @@ define(function(require) {
 	CKEDITOR.plugins.addExternal('indentblock', elgg.get_site_url() + 'mod/tgsutilities/views/default/js/elgg/ckeditor/indentblock/plugin.js', '');
 
 	return {
-		toolbar: [['Bold', 'Italic', 'Underline', 'Strike', 'RemoveFormat'], ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'Indent', 'Outdent'],[ 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize'], ['Format', 'FontSize', 'TextColor', 'BGColor']],
+		toolbar: [['Bold', 'Italic', 'Underline', 'Strike', 'RemoveFormat'], ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'Indent', 'Outdent'],[ 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'Maximize'], ['Format', 'FontSize', 'TextColor', 'BGColor']],
 		removeButtons: 'Subscript,Superscript', // To have Underline back
 		//allowedContent: true,
 		baseHref: elgg.config.wwwroot,
@@ -28,6 +28,10 @@ define(function(require) {
 		disableNativeTableHandles: false,
 		removeDialogTabs: 'image:advanced;image:Link;link:advanced;link:target',
 		autoGrow_maxHeight: $(window).height() - 100,
-		startupFocus: true
+		startupFocus: true,
+		keystrokes: [
+			[ CKEDITOR.CTRL + 75 /*K*/, 'link' ],
+			[ CKEDITOR.CTRL + 76 /*L*/, 'link' ]
+		]
 	};
 });
